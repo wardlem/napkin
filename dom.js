be94b900c7776065664d005087a586f301e67c73
Mark Wardle <mark@loganwebdesign.com>
2014-02-28 17:47:51 -0700

    working on dom manipulation library.

---
+++ dom.js
@@ -0,0 +1,17 @@
+var Napkin = Napkin || {};
+
+(function(N){
+
+    if (!N.Class) return;
+
+    var select = function(selector, ctx){
+        var i, l;
+        if (typeof selector !== 'string') throw Error('Napkin.select(): Selector must be a string');
+        selector = selector.split(' ');
+        for (i = 0, l < selector.length; i < l; i++){
+            
+        }
+        ctx = ctx || Document;
+    }
+
+})(Napkin);
